8c64977acd3d40517db4942329b09b5595785fe3
YChoux-Pan <yichengpan9527@gmail.com>
2026-05-08 00:34:17 +0800

    增加qss样式

--- VisionPYC/src/Plugins/CameraInputWidget.cpp
+++ VisionPYC/src/Plugins/CameraInputWidget.cpp
@@ -5,9 +5,25 @@ CameraInputWidget::CameraInputWidget(QWidget *parent)
 {
 	ui.setupUi(this);
 
-	QFile file(":/style.qss");
-	if (file.open(QFile::ReadOnly)) {
-		this->setStyleSheet(file.readAll());
+	QString fullStyle;
+
+	// 加载第一个样式文件
+	QFile file1(":/style.qss");
+	if (file1.open(QFile::ReadOnly)) {
+		fullStyle.append(file1.readAll());
+		file1.close();
+	}
+
+	// 加载第二个样式文件
+	QFile file2(":/radiobutton.qss");
+	if (file2.open(QFile::ReadOnly)) {
+		fullStyle.append(file2.readAll());
+		file2.close();
+	}
+
+	// 统一应用
+	if (!fullStyle.isEmpty()) {
+		this->setStyleSheet(fullStyle);
 	}
 
 
